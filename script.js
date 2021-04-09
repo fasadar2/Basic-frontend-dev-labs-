138017e16753513a0337701ecc34ff6050b84e19
fasadar2 <orlov.maksim.02@gmail.com>
2021-04-09 13:21:21 +0300

    ddt

---
+++ script.js
@@ -0,0 +1,16 @@
+function button() {
+
+    Swal.fire( {
+        allowEscapeKey:'true',
+        background:'#dfc6eb',
+        title:'Внимание, кнопка нажата',
+        iconColor:'#cc00cc',
+        icon:'info',
+       text:'Макет был реализован Орлов Максим Андреевич. Вариант № 5',
+       timer:'3453',
+       timerProgressBar:'true',
+       confirmButtonColor:'#cc00cc',
+       confirmButtonText:'Ok'
+    })
+
+} 
